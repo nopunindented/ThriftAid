83ed6fddac853962efbdf17aa5d7e9b868990431
umerindented <umerfiaz251@gmail.com>
2023-05-18 23:20:05 -0600

    "Navbar basic up"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import './Homepage/Navbar';
+import Navbar from './Homepage/Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ my-app/src/Homepage/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar () {
+    return(
+        <nav>
+            <h1>Hello</h1>
+        </nav>
+    )
+}
