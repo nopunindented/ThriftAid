f6457d6556c886b3a213b755249cf64cf7ba51b2
umerindented <umerfiaz251@gmail.com>
2023-05-22 19:33:57 -0600

    "Signup box barebones"

--- my-app/src/pages/Signupp.js
+++ my-app/src/pages/Signupp.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function SignupPage() {
     return (
         <div className="signup">
-            <h1>Hi</h1>
+            <div className="signupbox">
+
+            </div>
         </div>
     )
 }
