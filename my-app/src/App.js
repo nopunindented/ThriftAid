16c375a39bd99111f6824a9f30dbd4fd1d909952
umerindented <umerfiaz251@gmail.com>
2023-05-24 22:37:49 -0600

    "Signup and Login pages created"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,20 @@ import './navbar/Navbar'
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './navbar/Navbar';
 import Home from './pages/Home'
-import SignupPage from './pages/Signupp';
 import { useLocation } from 'react-router-dom';
 import NavbarMaybe from './navmaybe';
 import Dashboard from './pages/userdashboard';
+import Sign from './pages/signuppageform/Signform';
+import Loggedin from './pages/loginpage/Loginpage';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route exact path="/" Component={Home} />
-        <Route exact path= "/signup" Component={SignupPage} />
+        <Route exact path= "/signup" Component={Sign} />
         <Route exact path= "/dashboard" Component={Dashboard} />
+        <Route exact path= "/login" Component={Loggedin} />
       </Routes>
       <NavbarMaybe />
     </div>
---
+++ my-app/src/pages/loginpage/Loginpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Userfront from "@userfront/react";
+
+Userfront.init("rbv5jmqn");
+
+const LoginForm = Userfront.build({
+  toolId: "aladarr"
+});
+
+export default function Loggedin() {
+  return <LoginForm />;
+}
