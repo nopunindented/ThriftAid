287c755968657703c52d222aad013587ba396883
umerindented <umerfiaz251@gmail.com>
2023-05-28 21:37:07 -0600

    "custom signup form in good progress"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home'
 import { useLocation } from 'react-router-dom';
 import NavbarMaybe from './navmaybe';
 import Dashboard from './pages/userdashboard';
-import Sign from './pages/signuppageform/Signform';
+import SignupForm from './pages/signuppageform/Signform';
 import Loggedin from './pages/loginpage/Loginpage';
 import Userfront from "@userfront/core";
 
@@ -16,7 +16,7 @@ function App() {
     <div className='App'>
       <Routes>
         <Route exact path="/" Component={Home} />
-        <Route exact path= "/signup" Component={Sign} />
+        <Route exact path= "/signup" Component={SignupForm} />
         <Route exact path= "/dashboard" Component={Dashboard} />
         <Route exact path= "/login" Component={Loggedin} />
       </Routes>
