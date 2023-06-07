27fc8c4a773e0ea9ccde1ab8f7e20941c66dd54a
umerindented <umerfiaz251@gmail.com>
2023-06-06 19:04:41 -0600

    "server appears to work"

--- ThriftAidOverall/server/config/keys.js
+++ ThriftAidOverall/server/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://mufiaz:skIF1Y525tbDj9mT@thriftaid.oankunu.mongodb.net/?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://mufiaz:skIF1Y525tbDj9mT@thriftaid.oankunu.mongodb.net/?retryWrites=true&w=majority",
+    secretOrKey: "secret"
   };
--- ThriftAidOverall/server/server.js
+++ ThriftAidOverall/server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 const app = express();
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -19,5 +22,14 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+  
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server is good to go on ${port} !`));
