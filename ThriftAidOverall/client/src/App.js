b9d611b18d68f5e3d68d29634ff3be7a222ac469
umerindented <umerfiaz251@gmail.com>
2023-06-19 21:23:42 -0600

    "routing errors with /profile"

--- ThriftAidOverall/client/src/App.js
+++ ThriftAidOverall/client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
@@ -10,9 +10,9 @@ import Home from './pages/Home.js';
 import Register from './auth/Signup';
 import Dashboard from './pages/dashboard/Dashboard';
 import UserNavbar from './loggedinnavbar/UserNavbar.js';
+import Profile from './pages/dashboard/Profile';
 
 function App() {
-
   useEffect(() => {
     // Check if the user is logged in on each App render
     const jwtToken = localStorage.getItem('jwtToken');
@@ -24,11 +24,18 @@ function App() {
       const currentTime = Date.now() / 1000;
       if (decoded.exp < currentTime) {
         store.dispatch(logoutUser());
-        window.location.href = '/login';
       }
     }
   }, []);
 
+  const isAuthenticated = store.getState().auth.isAuthenticated;
+
+  const ProtectedRoute = ({ path, element }) => {
+    if (!isAuthenticated) {
+      return <Navigate to="/login" replace />;
+    }
+    return element;
+  };
 
   return (
     <Provider store={store}>
@@ -37,9 +44,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+          <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
         </Routes>
-      <UserNavbar />
+        <UserNavbar />
       </div>
     </Provider>
   );
---
+++ ThriftAidOverall/client/src/pages/dashboard/Profile.js
@@ -0,0 +1,110 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+
+
+const Profile = ({ auth, logoutUser }) => {
+  console.log(auth);
+  const navigate = useNavigate();
+  const { user, isAuthenticated } = auth;
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate]);
+
+  const onLogoutClick = (e) => {
+    e.preventDefault();
+    logoutUser();
+    navigate("/login");
+  };
+
+  if (!isAuthenticated) {
+    return null; // or a loading spinner or any other indication
+  }
+
+  if (!user || !user.email) {
+    console.log(user); // Add this line
+    console.log(user && user.email && user.usertype);
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Hey there,</b> User
+            </h4>
+            <p className="flow-text grey-text text-darken-1">
+              An error occurred while retrieving user data.
+            </p>
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem",
+              }}
+              onClick={onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  else if(user.usertype==='thrift store'){
+  return (
+    <div>
+      <div className="pastpostings">
+        Your past postings:
+      </div>
+      
+      <Button
+        type="submit"
+        sx={{
+          position: "absolute",
+          display: "flex",
+          color: "#F7F3F3",
+          fontFamily: "Noto Sans",
+          fontSize: 15,
+          fontStyle: "normal",
+          fontWeight: 700,
+          textAlign: "center",
+          height: 30,
+          left: "40%",
+          top: "55%",
+          width: 317,
+          textTransform: "none",
+          bgcolor: "#24a0ed",
+          ":hover": {
+            bgcolor: "#0792e8",
+            color: "#F7F3F3",
+            textTransform: "none"
+          }
+        }}
+        >
+          Create a posting
+        </Button>
+
+
+    </div>
+  )
+};
+}
+
+Profile.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logoutUser })(Profile);
