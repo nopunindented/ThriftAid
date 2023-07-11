347577821f8d964dc6292968e65869af58ac8900
umerindented <umerfiaz251@gmail.com>
2023-07-10 22:21:49 -0600

    "can display 4 postings per line"

--- ThriftAidOverall/client/src/pages/postings/AllPostings.js
+++ ThriftAidOverall/client/src/pages/postings/AllPostings.js
@@ -20,13 +20,8 @@ export default function AllPostings() {
       <h1 className='allpostings-container'>All Postings</h1>
       <div className='postings-container'>
         {postings.map((postin) => (
-          <div key={postin.thriftstore} className='allpostingcontainer'>
-            <h1 className='thriftstorepost'>{postin.thriftstore}</h1>
-            <h3>{postin.city}</h3>
-            <h3>{postin.country}</h3>
-            <h3>{postin.pickuptime}</h3>
-            <h3>{postin.pickupdate}</h3>
-            <h3>{postin.numberofphone}</h3>
+          <div key={postin.thriftstore} className='thriftstorepost'>
+            {postin.thriftstore}
           </div>
         ))}
       </div>
