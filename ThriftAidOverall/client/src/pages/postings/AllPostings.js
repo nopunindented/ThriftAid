8652bef2041bf4ca5a08531cd9eff9fa7b95a843
umerindented <umerfiaz251@gmail.com>
2023-07-20 20:36:52 -0600

    "auto resize and conditional comments button"

--- ThriftAidOverall/client/src/pages/postings/AllPostings.js
+++ ThriftAidOverall/client/src/pages/postings/AllPostings.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Dialog, Button,  DialogContent } from '@mui/material';
 
 export default function AllPostings() {
   const [postings, setPostings] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleDialogOpen = () => {
+    setOpenDialog(true);
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+  };
+
 
   useEffect(() => {
     axios
@@ -24,6 +35,50 @@ export default function AllPostings() {
             <div className='thriftstorepostfont'>{postin.thriftstore}</div>
             <div className="address-top">{postin.address}</div>
             <div className="city-top">{postin.city}, {postin.country}</div>
+            {postin.pickupcomments && (
+            
+            <>
+            <Button
+            type="submit"
+            sx={{
+              position: 'absolute',
+              display: 'flex',
+              color: '#F7F3F3',
+              fontFamily: 'Noto Sans',
+              fontSize: "1.5vh",
+              fontStyle: 'normal',
+              fontWeight: 700,
+              textAlign: 'center',
+              height: 30,
+              left: '15.7%',
+              top: '91%',
+              width: '70%',
+              textTransform: 'none',
+              bgcolor: "#5ab0f2",
+              ":hover": {
+                bgcolor: "#4baaf2",
+                color: "#F7F3F3",
+                textTransform: "none"
+              }
+            }}
+            onClick={handleDialogOpen}
+          >
+            View poster's comments
+          </Button>
+        <Dialog open={openDialog} onClose={handleDialogClose} className='dialoguebackgroundtwice'>
+        <div className="dialoguecancel-container">
+          <Button onClick={handleDialogClose} variant="contained" color="primary" className='dialoguecancel'>
+              Cancel
+          </Button>
+        </div>
+          <DialogContent>
+            <div className='postercomments'>
+            {postin.pickupcomments}
+            </div>
+          </DialogContent>
+        </Dialog>
+        </>
+         )}
           </div>
         ))}
       </div>
