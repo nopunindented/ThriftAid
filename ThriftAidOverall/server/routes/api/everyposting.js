a506e70c4fc3fc008cf32b70f2b983072b5f8c76
umerindented <umerfiaz251@gmail.com>
2023-07-28 09:26:57 -0600

    "created new list of accepted"

--- ThriftAidOverall/server/routes/api/everyposting.js
+++ ThriftAidOverall/server/routes/api/everyposting.js
@@ -11,6 +11,7 @@ router.get('/allpostings', passport.authenticate('jwt', { session: false }), asy
   
     try {
       const postings = await Posting.find({});
+      
       res.json(postings);
     } catch (err) {
       console.log(err);
@@ -18,6 +19,29 @@ router.get('/allpostings', passport.authenticate('jwt', { session: false }), asy
     }
   });
 
+  router.post('/acceptposting', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    if (req.user.usertype !== 'homeless shelter') {
+      return res.status(403).json({ error: 'Only homeless shelters can accept postings' });
+    }
+  
+    try {
+      const { postinId } = req.body;
+      const acceptedPosting = await Posting.findById(postinId);
+      if (!acceptedPosting) {
+        return res.status(404).json({ error: 'Posting not found' });
+      }
+
+      await acceptedPosting.remove();
+  
+      const updatedAcceptedPostings = await Posting.find({});
+      AcceptedPostings = updatedAcceptedPostings;
+  
+      res.json(updatedAcceptedPostings);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Server Error' });
+    }
+  });
 
 
   module.exports = router;
