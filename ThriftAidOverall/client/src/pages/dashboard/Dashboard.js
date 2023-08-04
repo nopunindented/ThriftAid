1a3d9d0c1d339aaaf74b4078fc78ec9558b2ff99
umerindented <umerfiaz251@gmail.com>
2023-08-03 21:18:29 -0600

    "finished accepted posts"

--- ThriftAidOverall/client/src/pages/dashboard/Dashboard.js
+++ ThriftAidOverall/client/src/pages/dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Button } from "@mui/material";
+import { Button, Dialog, DialogContent } from "@mui/material";
 import GoogleMaps from "../postings/googlemaps";
 import Logo from "./PostingLogo";
 import mapmarker from "../postings/mapmarker.svg"
@@ -12,13 +12,25 @@ import clock from "../postings/clock.svg"
 import website from "../postings/internet.svg"
 import emailicon from "../postings/email.svg"
 import phoneicon from "../postings/phone.svg"
+import xmark from "../postings/xmark.png"
 
 const Dashboard = ({ auth, logoutUser }) => {
   const navigate = useNavigate();
   const { user, isAuthenticated } = auth;
   const [acceptedposts, setAcceptedPosts] = useState(null);
   const [selectedPostAddress, setSelectedPostAddress] = useState(null);
+  const [selectedPostin, setSelectedPostin] = useState(null);
 
+  const dialogStyle = {
+    width: '20%',
+    height: '40%',
+    backgroundColor: '#f5f2f2',
+    overflowY: 'auto',
+  };
+
+  const handleDialogOpen = (postin) => {
+    setSelectedPostin(postin);
+  };
 
   const handleThriftPostClick = (address, city) => {
     setSelectedPostAddress(address, city);
@@ -28,6 +40,87 @@ const Dashboard = ({ auth, logoutUser }) => {
     setSelectedPostAddress(address, city);
   };
 
+  const handleDialogClose = () => {
+    setSelectedPostin(null);
+  };
+
+  const pickupaccept = {
+    position: 'absolute',
+    display: 'flex',
+    color: '#F7F3F3',
+    fontFamily: 'Noto Sans',
+    fontSize: '1.4vh',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    textAlign: 'center',
+    top: "84.5%",
+    height: '7%',
+    left: '21.7%',
+    width: '55%',
+    textTransform: 'none',
+    bgcolor: '#5ab0f2',
+    ':hover': {
+      bgcolor: '#4baaf2',
+      color: '#F7F3F3',
+      textTransform: 'none',
+    },
+  };
+
+  const buttonStyle = {
+    position: 'absolute',
+    display: 'flex',
+    color: '#F7F3F3',
+    fontFamily: 'Noto Sans',
+    fontSize: '1.4vh',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    textAlign: 'center',
+    top: "91%",
+    height: '7%',
+    left: '21.7%',
+    width: '55%',
+    textTransform: 'none',
+    bgcolor: '#5ab0f2',
+    ':hover': {
+      bgcolor: '#4baaf2',
+      color: '#F7F3F3',
+      textTransform: 'none',
+    },
+  };
+
+  const pickupbutton = {
+    position: 'absolute',
+    display: 'flex',
+    color: '#F7F3F3',
+    fontFamily: 'Noto Sans',
+    fontSize: '1.4vh',
+    fontStyle: 'normal',
+    fontWeight: 520,
+    textAlign: 'center',
+    top: "80%",
+    height: '16%',
+    left: '61.5%',
+    width: '38%',
+    textTransform: 'none',
+    bgcolor: '#5e5e5e',
+    ':hover': {
+      bgcolor: '#424242',
+      color: '#F7F3F3',
+      textTransform: 'none',
+    },
+  };
+  
+  const ButtonAcceptPostingTwo = ({ onClick }) => (
+    <Button sx={pickupaccept} onClick={onClick}>
+      Accept Posting
+    </Button>
+  );
+  
+  const ButtonViewComments = ({ onClick }) => (
+    <Button sx={pickupbutton} onClick={onClick}>
+      View poster's comments
+    </Button>
+  );
 
   const userAcceptedPosts = acceptedposts && acceptedposts.filter(
     (post) => post.posting.email === user.email
@@ -55,12 +148,37 @@ const Dashboard = ({ auth, logoutUser }) => {
             <div className="thrifted-name">{post.posting.thriftstore}</div>
             <div className="address-dashboard">{post.posting.address}</div>
             <div className="address-dashboard-city">{post.posting.city}, {post.posting.country}</div>
+            <div className="time-dashboard-city">{post.posting.pickuptime}, {post.posting.pickupdate}</div>
+            <div className="website-dashboard-city">{post.posting.website}</div>
+            <div className="email-dashboard-city">{post.posting.email}</div>
+            <div className="phone-dashboard-city">{post.posting.numberofphone}</div>
             <img src= {mapmarker} className="map-dashboard" />
             <img src= {clock} className="clock-dashboard" />
             <img src= {website} className="website-dashboard" />
             <img src= {phoneicon} className= "phone-dashboard" />
             <img src= {emailicon} className="email-dashboard" />
-            {post.posting.email}
+            {post.posting.pickupcomments && (
+          <>
+            <ButtonViewComments onClick={() => handleDialogOpen(post.posting)} />
+            <Dialog
+              open={selectedPostin === post.posting}
+              onClose={handleDialogClose}
+              className='dialoguebackgroundtwice'
+              PaperProps={{
+                style: dialogStyle,
+              }}
+            >
+              <DialogContent>
+                <Button onClick={handleDialogClose}>
+                  <img src={xmark} className="xmarkdialogue" alt="Close" />
+                </Button>
+                <div className='postercomments'>
+                  {post.posting.pickupcomments}
+                </div>
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
           </div>
         ), [userAcceptedPosts])
   )
