b667f40a1329f2b6c5a059319788399b607af274
umerindented <umerfiaz251@gmail.com>
2023-08-06 15:59:06 -0600

    "home page dynamic resizing"

--- ThriftAidOverall/client/src/pages/homepagesetup/homebody.js
+++ ThriftAidOverall/client/src/pages/homepagesetup/homebody.js
@@ -7,9 +7,8 @@ export default function Homebody () {
                 <h1 className="hometextmain">ThriftAid</h1>
                 <p className="hometextsecondary">
                 ThriftAid is an application that links thrift stores and homeless shelters, 
-                with the goal of reducing waste. Thrift stores with excess clothes can post a listing
-                (with options for pick up or delivery),
-                and a nearby homeless shelter can choose to accept it. 
+                with the goal of reducing waste. Thrift stores with excess clothes can post a listing,
+                and a homeless shelter on the platform can choose to accept it. 
                 </p>
                 <ellipse className="ellipse1" />
                 <ellipse className="ellipse2" />
