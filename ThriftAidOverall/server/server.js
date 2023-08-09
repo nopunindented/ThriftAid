343348fff36879b4da9efb39c4967e8a117a4b95
umerindented <umerfiaz251@gmail.com>
2023-08-08 20:05:35 -0600

    "attempt to fix cors"

--- ThriftAidOverall/server/server.js
+++ ThriftAidOverall/server/server.js
@@ -64,9 +64,13 @@ app.get("/", (req, res) => {
 });
 
 
-app.use(cors({
-  origin: "https://thriftaidforall.com/"
-}));
+// Replace the current cors configuration with a simple one
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.get("/create", async (req, res) => {
   try {
